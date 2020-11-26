195ed39b7fecb2618cc5f49e6f90cbc66bf36d4a
zavtra1000 <thecalifornia@ya.ru>
2020-11-26 08:44:31 +0300

    create: added reviews section & fix hotel slide

--- js/main.js
+++ js/main.js
@@ -23,8 +23,8 @@ var reviewsSlider = new Swiper('.reviews-slider', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
   },
   
 })
