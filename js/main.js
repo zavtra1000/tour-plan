0009009c7cf0a023cb920054e1566627d002179e
zavtra1000 <thecalifornia@ya.ru>
2020-12-01 21:31:29 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -1,43 +1,44 @@
-var hotelSwiper = new Swiper('.hotel-slider', {
+var hotelSwiper = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
   },
-  
-})
+});
 
-var hotelSwiper = new Swiper('.hotel-slider', {
+var hotelSwiper = new Swiper(".hotel-slider", {
   keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
 });
 
-var reviewsSlider = new Swiper('.reviews-slider', {
+var reviewsSlider = new Swiper(".reviews-slider", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
   },
-  
-})
+});
 
-var reviewsSlider = new Swiper('.reviews-slider', {
+var reviewsSlider = new Swiper(".reviews-slider", {
   keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
 });
 
-$('.newletter').parallax({imageSrc: '../img/newletter.jpg'});
+$(".newletter").parallax({ imageSrc: "../img/newletter.jpg" });
 
-  
-
-  
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  document
+    .querySelector(".navbar-botton")
+    .classList.toggle("navbar-botton--visible");
+});
